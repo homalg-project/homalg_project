479693d7c9d087f61acf3a9e20d33fdf65400569
Florian Diebold <flodiebold@gmail.com>
2011-07-26 18:52:14 +0200

    Some documentation for ExteriorAlgebra.

--- gap/ExteriorAlgebra.gd
+++ gap/ExteriorAlgebra.gd
@@ -8,6 +8,18 @@
 ##
 #############################################################################
 
+##  <#GAPDoc Label="ExteriorPowers">
+##  <ManSection>
+##    <Attr Arg="M" Name="ExteriorPowers"/>
+##    <Returns>a record containing the cached exterior powers</Returns>
+##    <Description>
+##      This stores the exterior powers of the modules that have been generated
+##      so far. <Br />
+##      Use <C>ExteriorPower</C> to access exterior powers.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareAttribute( "ExteriorPowers",
         IsHomalgModule, "mutable" );
 
@@ -15,16 +27,56 @@ DeclareOperation( "ExteriorPower",
         [ IsInt, IsHomalgModule ]);
 
 
+##  <#GAPDoc Label="IsExteriorPower">
+##  <ManSection>
+##    <Prop Arg="M" Name="IsExteriorPower"/>
+##    <Returns><C>true</C> or <C>false</C></Returns>
+##    <Description>
+##      Marks a module as an exterior power of another module.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareProperty( "IsExteriorPower",
         IsHomalgModule );
 
+##  <#GAPDoc Label="ExteriorPowerExponent">
+##  <ManSection>
+##    <Attr Arg="M" Name="ExteriorPowerExponent"/>
+##    <Returns>an integer</Returns>
+##    <Description>
+##      The exponent of the exterior power.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareAttribute( "ExteriorPowerExponent",
         IsHomalgModule );
 
+##  <#GAPDoc Label="ExteriorPowerBaseModule">
+##  <ManSection>
+##    <Attr Arg="M" Name="ExteriorPowerBaseModule"/>
+##    <Returns>a homalg module</Returns>
+##    <Description>
+##      The module that <A>M</A> is an exterior power of.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareAttribute( "ExteriorPowerBaseModule",
         IsHomalgModule );
 
 
+##  <#GAPDoc Label="IsExteriorPowerElement">
+##  <ManSection>
+##    <Prop Arg="x" Name="IsExteriorPowerElement"/>
+##    <Returns><C>true</C> or <C>false</C></Returns>
+##    <Description>
+##      Checks if the element <A>x</A> is from an exterior power.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareProperty( "IsExteriorPowerElement",
         IsHomalgModuleElement );
 
@@ -47,6 +99,16 @@ DeclareOperation( "GradeSequence",
         [ IsList, IsHomalgModule ] );
 
 
+##  <#GAPDoc Label="CayleyDeterminant">
+##  <ManSection>
+##    <Attr Arg="C" Name="CayleyDeterminant"/>
+##    <Returns>a ring element</Returns>
+##    <Description>
+##      Returns the Cayley determinant of the complex <A>C</A>, if it is defined.
+##    </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+##
 DeclareAttribute( "CayleyDeterminant",
         IsHomalgComplex );
 
