2976fb789ef6595d76c70e459b7f262754f15530
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-10-30 13:56:05 +0100

    made documenation to be build in correct folder. Important for autodoc

--- make_dist_for_homalg.sh
+++ make_dist_for_homalg.sh
@@ -24,7 +24,10 @@ EOF
 
   version=$(cat VERSION)
   rm VERSION
-  gap makedoc.g
+  gap -A <<GAPInput
+SetPackagePath("${i}", ".");
+Read("makedoc.g");
+GAPInput
   rm -rf .git*
   rm -f doc/*.{aux,bbl,blg,brf,idx,ilg,ind,lab,log,out,pnr,tex,toc,tst}
   rm -rf bin/
