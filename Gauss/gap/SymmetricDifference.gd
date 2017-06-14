b49784d9febd9127b72498ec63258a4bb94d2008
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2017-06-14 14:37:34 +0200

    Fallback function for Gauss C-module

---
+++ Gauss/gap/SymmetricDifference.gd
@@ -0,0 +1,11 @@
+#############################################################################
+##
+##  SymmetricDifference.gd     Gauss package                Sebastian Gutsche
+##
+##  Copyright 2017, University of Siegen
+##
+##  Fallback for symmetric difference operation
+##
+#############################################################################
+
+DeclareGlobalFunction( "SYMMETRIC_DIFFERENCE_OF_ORDERED_SETS_OF_SMALL_INTEGERS" );
