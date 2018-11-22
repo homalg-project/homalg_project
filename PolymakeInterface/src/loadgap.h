f7785487552829bf4334ff66dda32850339adf05
Max Horn <max@quendi.de>
2018-11-22 14:47:49 +0100

    Simplify PolymakeInterface/src/loadgap.h

--- PolymakeInterface/src/loadgap.h
+++ PolymakeInterface/src/loadgap.h
@@ -1,32 +1,11 @@
 #ifndef LOADGAP_H
 #define LOADGAP_H
 
-#include <cstddef>
-
-/* Try to use as much of the GNU C library as possible: */
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
-//Included by Thomas to work
-#include<gmp.h>
-
+// include GAP headers
 extern "C" {
-
-
-#include <src/compiled.h>          /* GAP headers                */
-
-
-
-/* The following seems to be necessary to run under modern gcc compilers
- * which have the ssp stack checking enabled. Hopefully this does not
- * hurt in future or other versions... */
-#ifdef __GNUC__
-#if (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1))
-extern void __stack_chk_fail();
-#endif
-#endif
+#include <src/compiled.h>
 }
 
 #endif
-# define MORE_TESTS 1
+
+#define MORE_TESTS 1
