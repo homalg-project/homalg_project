homalg-project/homalg_project

commit 28db73734e94d6b6777545f4b6b036b13affde12

Fixed bug in groebner basis term order

4ti2Interface/PackageInfo.g
@@ -7,7 +7,7 @@ PackageName := "4ti2Interface",
 Subtitle := "A link to 4ti2",
 
 Version := Maximum( [
-  "2015.04.29", ## Sebas' version
+  "2017.01.05", ## Sebas' version
 ## this line prevents merge conflicts
 "2015.11.06", ## Homepage update version, to be removed
 ] ),

4ti2Interface/gap/4ti2Interface.gi
@@ -2,7 +2,7 @@
 ##
 ##                                                      4ti2Interface package
 ##
-##  Copyright 2013,           Sebastian Gutsche, University of Kaiserslautern
+##  Copyright 2013-2017,                 Sebastian Gutsche, Siegen University
 ##
 ##  Reading the declaration part of the 4ti2Interface package.
 ##
@@ -161,7 +161,11 @@ InstallGlobalFunction( 4ti2Interface_groebner_matrix,
     
     if Length( arg ) > 1 then
         
-        4ti2Interface_Write_Matrix_To_File( [ arg[ 2 ] ], Concatenation( filename, ".cost" ) );
+        if not IsMatrix( arg[ 2 ] ) then
+            arg[ 2 ] := [ arg[ 2 ] ];
+        fi;
+        
+        4ti2Interface_Write_Matrix_To_File( arg[ 2 ], Concatenation( filename, ".cost" ) );
         
     fi;
     
@@ -202,6 +206,10 @@ InstallGlobalFunction( 4ti2Interface_groebner_basis,
     4ti2Interface_Write_Matrix_To_File( matrix, Concatenation( filename, ".lat" ) );
     
     if Length( arg ) > 1 then
+        
+        if not IsMatrix( arg[ 2 ] ) then
+            arg[ 2 ] := [ arg[ 2 ] ];
+        fi;
         
         4ti2Interface_Write_Matrix_To_File( [ arg[ 2 ] ], Concatenation( filename, ".cost" ) );
         

commit 0a4a4228fa92ce5aee3f22c902b5338a6a84584d

fixed bug introduced in commit 28db7373

4ti2Interface/PackageInfo.g
@@ -9,7 +9,7 @@ Version := Maximum( [
 ## this line prevents merge conflicts
   "2018.07.06", ## Kamal's version
 ## this line prevents merge conflicts
-  "2019.09.03", ## Mohamed's version
+  "2019.09.04", ## Mohamed's version
 ] ),
 
 Date := ~.Version{[ 1 .. 10 ]},

4ti2Interface/gap/4ti2Interface.gi
@@ -219,7 +219,7 @@ InstallGlobalFunction( 4ti2Interface_groebner_basis,
             arg[ 2 ] := [ arg[ 2 ] ];
         fi;
         
-        4ti2Interface_Write_Matrix_To_File( [ arg[ 2 ] ], Concatenation( filename, ".cost" ) );
+        4ti2Interface_Write_Matrix_To_File( arg[ 2 ], Concatenation( filename, ".cost" ) );
         
     fi;
     